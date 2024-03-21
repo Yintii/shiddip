de68512ba81d9d773a0ccddd6c6a2cd4c5d88a7f
yintii <keleheart1@gmail.com>
2024-03-20 23:46:14 -0700

    geocoding address and pin point

--- app/javascript/custom/map.js
+++ app/javascript/custom/map.js
@@ -1,13 +1,25 @@
 import mapboxgl from "mapbox-gl/dist/mapbox-gl.js"
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
+
 
 document.addEventListener('turbo:load', function(){
     mapboxgl.accessToken = 'pk.eyJ1IjoieWludGlpIiwiYSI6ImNsdTBxaHNmZzAxbnoyanBkcnppbnlmYXYifQ.u5AfLGU61hYzl1Yk8wuPYg';
 
-    let map = new mapboxgl.Map({
+    const map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/mapbox/streets-v11',
         center: [-74.5, 40],
         zoom: 9
     });
+
+    const geocoder = new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        marker: {
+            color: 'orange'
+        },
+        mapboxgl: mapboxgl
+    });
+
+    document.getElementById('search-bar').appendChild(geocoder.onAdd(map));
 
 })
